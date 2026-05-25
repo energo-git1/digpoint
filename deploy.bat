d1a3161cafe076214900c5236d3f79ffefd7260b
Eimutis <github@energolt.eu>
2026-05-25 13:28:34 +0300

    Paraiškos

---
+++ deploy.bat
@@ -0,0 +1,12 @@
+@echo off
+cd /d %~dp0
+echo [1/3] git add + commit...
+git add -A
+git commit -m "v1.2.88: EsoSubmitBtn single-click - remove modal, clean unused state"
+echo [2/3] git push...
+git push
+echo [3/3] Triggering server deploy (git pull + pm2 restart)...
+curl -s -X POST http://10.2.1.115:3001/api/admin/deploy
+echo.
+echo Done! Reload the Digpoint tab to see v1.2.88
+pause
