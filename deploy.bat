c6750a6461823eb9968008c55481de075a4f34bc
Eimutis <github@energolt.eu>
2026-05-25 15:33:51 +0300

    Telia

--- deploy.bat
+++ deploy.bat
@@ -2,11 +2,11 @@
 cd /d %~dp0
 echo [1/3] git add + commit...
 git add -A
-git commit -m "v1.2.95: Papildoma paraiška - greitas tam pačiam užsakovui su užpildytais duomenimis"
+git commit -m "v1.2.96: Išsaugomi pasirinkti PDF puslapiai (Telia/KE/TeliaInv) paspaudus Išsaugoti"
 echo [2/3] git push...
 git push
 echo [3/3] Triggering server deploy (git pull + pm2 restart)...
 curl -s -X POST http://10.2.1.115:3001/api/admin/deploy
 echo.
-echo Done! Reload the Digpoint tab to see v1.2.95
+echo Done! Reload the Digpoint tab to see v1.2.96
 pause
