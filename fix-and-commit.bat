44857542b013257bb04c217b64adff5d288d799a
Žiedė <ziede.rumsaite@energolt.eu>
2026-06-05 10:30:39 +0300

    Revert "Update fix-and-commit.bat"
    This reverts commit e6c51bb65139b27279dc57fb3e5f33c2fb9d6f7e.

--- fix-and-commit.bat
+++ fix-and-commit.bat
@@ -1,31 +1,14 @@
 @echo off
 cd /d "%~dp0"
-
-echo Ieškau git...
-for /f "delims=" %%i in ('where git 2^>nul') do set GIT="%%i" & goto :found
-for /f "delims=" %%i in ('dir /b /s "C:\Users\eimutis.simkus\AppData\Local\GitHubDesktop\*.exe" 2^>nul ^| findstr /i "\\git\\cmd\\git.exe"') do set GIT="%%i" & goto :found
-for /f "delims=" %%i in ('dir /b /s "C:\Program Files\Git\cmd\git.exe" 2^>nul') do set GIT="%%i" & goto :found
-echo GIT NERASTAS. Skambink Eimučiui.
-pause
-exit /b 1
-
-:found
-echo Rastas: %GIT%
-echo.
-
-echo Šalinu git užraktą...
+echo Salinu git uzrakta...
 del /f .git\index.lock 2>nul
-
 echo Ruošiu failus...
-%GIT% add energolt-kasimo.user.js server.js package.json
-%GIT% restore public\index.html
-
+git add energolt-kasimo.user.js server.js package.json
+git restore public\index.html
 echo Darau commit...
-%GIT% commit -m "v1.5.58: SAV parsavimas, municipality ištraukimas, ESO retry ciklas"
-
-echo Siunčiu į GitHub...
-%GIT% push
-
+git commit -m "v1.5.58: SAV parsavimas, municipality ištraukimas, ESO retry ciklas"
+echo Siunciu i GitHub...
+git push
 echo.
-echo === ATLIKTA. Serveris atsistatys per ~1 min. ===
+echo ATLIKTA. Galite uždaryti langą.
 pause
